39ef0a3bace1e499e633c8e91dc52ffc8e05f720
wilson Neto <wngaspar10@gmail.com>
2021-12-18 16:43:11 -0300

    feat(widget): criado novo projeto e adicionado as configurações e primeiras views

---
+++ widget/src/views/Playground/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="w-full h-3/4 flex flex-col justify-center items-center bg-brand-main">
+    <h1 class="text-6xl font-black text-brand-gray">Playground</h1>
+    <p class="text-2xl mt-3 font-regular text-brand-gray">Este é o playground, use para testar o widget.</p>
+  </div>
+  <div class="w-full h-3/4 flex flex-col justify-center items-center bg-brand-gray">
+    <h1 class="text-6xl font-black text-brand-graydark">Playground</h1>
+  </div>
+  <div class="w-full h-3/4 flex flex-col justify-center items-center bg-brand-main">
+    <h1 class="text-6xl font-black text-brand-gray">Playground</h1>
+  </div>
+  <div class="w-full h-3/4 flex flex-col justify-center items-center bg-brand-gray">
+    <h1 class="text-6xl font-black text-brand-graydark">Playground</h1>
+  </div>
+  <widget />
+</template>
+
+<script lang="ts">
+
+import Widget from '@/views/Widget/index.vue'
+import { defineComponent } from '@vue/runtime-core'
+
+export default defineComponent({
+  name: 'Playground',
+  components: {
+    Widget
+  }
+})
+</script>
